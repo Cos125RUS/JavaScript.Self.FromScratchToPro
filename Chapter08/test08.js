2a842e265dd49b4040ece3fe9a83df8582c0120d
Valerii_Panov <val250640@mail.ru>
2024-01-17 21:24:40 +1000

    fix

--- Chapter08/test08.js
+++ Chapter08/test08.js
@@ -1,41 +1,46 @@
-let to = setDate();
+class Timer {
+    date;
 
-function timer() {
-    let now = new Date();
-    let year = to.getFullYear() - now.getFullYear();
-    let month = to.getMonth() - now.getMonth();
-    let day = to.getDate() - now.getDate();
-    let hour = to.getHours() - now.getHours();
-    let minute = to.getMinutes() - now.getMinutes();
-    let second = to.getSeconds() - now.getSeconds();
-
-    if (second < 0) {
-        minute--;
-        second += 60;
-    }
-    if (minute < 0) {
-        hour--;
-        minute += 60;
-    }
-    if (hour < 0) {
-        day--;
-        hour += 24;
-    }
-    if (day < 0) {
-        month--;
-        day += 30; // TODO: привязку к конкретному месяцу
-    }
-    if (month < 0) {
-        year--;
-        month += 12;
+    constructor(date) {
+        this.date = date;
     }
 
-    console.log("year:", year, "  month:", month, "  day:", day, "  hour:", hour, "  minute:", minute, "  second:", second);
+    step() {
+        let now = new Date();
+        let year = this.date.getFullYear() - now.getFullYear();
+        let month = this.date.getMonth() - now.getMonth();
+        let day = this.date.getDate() - now.getDate();
+        let hour = this.date.getHours() - now.getHours();
+        let minute = this.date.getMinutes() - now.getMinutes();
+        let second = this.date.getSeconds() - now.getSeconds();
+
+        if (second < 0) {
+            minute--;
+            second += 60;
+        }
+        if (minute < 0) {
+            hour--;
+            minute += 60;
+        }
+        if (hour < 0) {
+            day--;
+            hour += 24;
+        }
+        if (day < 0) {
+            month--;
+            day += 30; // TODO: привязку к конкретному месяцу
+        }
+        if (month < 0) {
+            year--;
+            month += 12;
+        }
+
+        console.log("year:", year, "  month:", month, "  day:", day, "  hour:", hour, "  minute:", minute, "  second:", second);
+    }
+
+    run() {
+        setInterval(() => this.step(), 1000);
+    }
 }
 
-function setDate() {
-    return new Date("2024/5/6");  // Заглушка
-    // TODO: добавить ввод
-}
-
-setInterval(timer, 1000);
+let myTimer = new Timer(new Date("2024/5/6")).run();
