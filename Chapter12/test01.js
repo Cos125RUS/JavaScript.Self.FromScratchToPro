2be003dfd6e092be56167289fe914f12c7ec6e11
Valerii_Panov <val250640@mail.ru>
2024-02-11 09:31:39 +1000

    chapter12 task01

---
+++ Chapter12/test01.js
@@ -0,0 +1,27 @@
+let text1 = "I love JavaScript";
+let text2 = "I love Node.js";
+let text3 = "I love React";
+let text4 = "I love JavaScript and React";
+let regex = /javascript|node|react/gi;
+console.log(text1.match(regex));
+console.log(text2.match(regex));
+console.log(text3.match(regex));
+console.log(text4.match(regex));
+
+let d = "d";
+console.log(d.match(/[a-zA-Z0-9]/g));
+console.log(text1.match(/./g));
+console.log('node.js'.match(/\./g));
+
+console.log("js node react".match(/\s/g));
+console.log('12 years old'.match(/\d/g));
+console.log('In begin'.match(/\bin/gi));
+
+console.log(text2.match(/(love)\s(node)/gi));
+
+console.log(text1.match(/v?a/g));
+console.log("123123123".match(/(123)+/g));
+console.log("123123123".match(/(123)*/g));
+console.log("123123123".match(/(123){1,2}/g));
+
+console.log(text1.match(/(?<language>javascript)/i));
